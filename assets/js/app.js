1773827a1c441d41f063f75a783d72447e5a39bc
Morteza <mortazapor3@gmail.com>
2023-06-11 09:50:54 +0330

    Improve css and js codes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,55 +1,68 @@
-function moveRight(){
-    $('#list li:first-of-type').removeClass('first-child');
-    $('#list li:last-of-type').addClass('first-child');
-    $('#list li:last-of-type').insertAfter('#moveR');
+function moveRight() {
+	$("#list li:last-of-type").insertAfter("#moveR");
 }
-function moveLeft(){
-    $('#list li:first-of-type').removeClass('first-child');
-    $('#list li:last-of-type').addClass('first-child');
-    $('#list li:first-of-type').insertBefore('#moveL');
+
+function moveLeft() {
+	$("#list li:first-of-type").insertBefore("#moveL");
 }
+
+// setInterval(moveRight, 4000);
+
+$("#moveR").click(moveRight);
+$("#moveL").click(moveLeft);
+
+const clientElemWidth = $(".client").width();
+let clientsSectionTranslateX = clientElemWidth;
+
 setInterval(() => {
-    moveRight();
-}, 4000);
-$('#moveR').click(function (e) { 
-    e.preventDefault();
-    moveRight();
-});
-$('#moveL').click(function (e) { 
-    e.preventDefault();
-    moveLeft();
-});
-let trans = 1575;
-setInterval(() => {
-    if(trans === -1125){
-        trans =2025;
-    }
-    trans -= $('.client').width();
-    $('.clients-section').css({
-        transform : `translate3d(${trans}px,0,0)`
-    });
+	$(".clients-section").css(
+		"transform",
+		`translateX(${clientsSectionTranslateX}px)`
+	);
+
+	if (clientsSectionTranslateX / clientElemWidth === 3) {
+		clientsSectionTranslateX = 0;
+	} else {
+		clientsSectionTranslateX += clientElemWidth;
+	}
 }, 2000);
-$('.last-link').click(function () {
-    $('.search-section').toggle();
+
+$(".last-link").click(function() {
+	$(".search-section").toggle();
 });
 
-window.onscroll = function() {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop >1000) {
-        $('.footer-scroll-up').css('display','block');
-    } else {
-        $('.footer-scroll-up').css('display','none');
-    }
+window.addEventListener("scroll", handleScrollUp);
+
+function handleScrollUp() {
+	$(".footer-scroll-up").css(
+		"display",
+		document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000
+			? "block"
+			: "none"
+	);
 }
+
 function scrollToTop() {
-    $('body').css({ 'overflow': 'auto' });
-    const scroll = (document.body.scrollTop || document.documentElement.scrollTop);
-    if (scroll) {
-        window.requestAnimationFrame(scrollToTop);
-        window.scrollTo({ top: 0 }, scroll - scroll / 8);
-        $('body').css({ 'overflow': 'hidden' });
-    }
+	const scroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+	$("body").css({ overflow: "auto" });
+
+	if (scroll) {
+		window.requestAnimationFrame(scrollToTop);
+		window.scrollTo({ top: 0 }, scroll - scroll / 8);
+		$("body").css({ overflow: "hidden" });
+	}
 }
-$('.footer-scroll-up').click(function (e) { 
-    e.preventDefault();
-    scrollToTop();
+
+$(".footer-scroll-up").click(function(e) {
+	e.preventDefault();
+	scrollToTop();
 });
+
+window.onload = () => {
+	for (let i = 1; i <= 10; i++) {
+		$(
+			`<style>.last-projects-cards__card.card-${i}:hover::before { content : "Project ${i}"}</style>`
+		).appendTo("head");
+	}
+};
